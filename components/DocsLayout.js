0d6bb7601f8dd92c23ddc0f7e0bc24f0605902bf
Lee Robinson <lrobinson2011@gmail.com>
2020-08-11 09:59:45 -0500

    Rename to docs layout?

--- components/DocsLayout.js
+++ components/DocsLayout.js
@@ -4,7 +4,7 @@ import { Box } from '@chakra-ui/core';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
-const MDXLayout = ({ children }) => (
+const DocsLayout = ({ children }) => (
   <>
     <Navbar />
     <Box maxW="600px" mx="auto" px={8}>
@@ -14,4 +14,4 @@ const MDXLayout = ({ children }) => (
   </>
 );
 
-export default MDXLayout;
+export default DocsLayout;
