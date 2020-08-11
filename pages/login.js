2c312de6769c633930ec000351b1805bad84ed8e
Lee Robinson <lrobinson2011@gmail.com>
2020-08-10 20:57:14 -0500

    Move email login to it's own route.

--- pages/login.js
+++ pages/login.js
@@ -1,110 +1,64 @@
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
-  Stack,
-  Icon,
-  useToast
-} from '@chakra-ui/core';
-import { useForm } from 'react-hook-form';
-import { useState } from 'react';
-
+import { Box, Button, Flex, Stack, Icon } from '@chakra-ui/core';
 import { useAuth } from '@/lib/auth';
 import Page from '@/components/Page';
 
 const Login = () => {
-  const toast = useToast();
-  const [loading, setLoading] = useState(false);
-  const { signinWithEmail } = useAuth();
-  const { handleSubmit, register, errors } = useForm();
-
-  const onLogin = ({ email, pass }) => {
-    setLoading(true);
-    signinWithEmail(email, pass).catch((error) => {
-      setLoading(false);
-      toast({
-        title: 'An error occurred.',
-        description: error.message,
-        status: 'error',
-        duration: 5000,
-        isClosable: true
-      });
-    });
-  };
+  const auth = useAuth();
 
   return (
-    <Flex align="center" justify="center" h="100vh" backgroundColor="gray.100">
+    <Flex
+      align="center"
+      justify="center"
+      h="100vh"
+      backgroundColor={['white', 'gray.100']}
+    >
       <Stack
-        as="form"
         backgroundColor="white"
         borderRadius={[0, 8]}
-        errors={errors}
         maxWidth="400px"
-        onSubmit={handleSubmit((data) => onLogin(data))}
         px={8}
         py={12}
-        register={register}
         shadow={[null, 'md']}
         spacing={4}
         w="100%"
       >
         <Flex justify="center">
           <Box as="a" href="/" aria-label="Back to homepage">
-            <Icon color="black" name="logo" size="64px" mb={4} />
+            <Icon color="black" name="logo" size="56px" mb={4} />
           </Box>
         </Flex>
-        <FormControl isInvalid={errors.email && errors.email.message}>
-          <FormLabel>Email Address</FormLabel>
-          <Input
-            autoFocus
-            aria-label="Email Address"
-            id="email"
-            name="email"
-            ref={register({
-              required: 'Please enter your email.'
-            })}
-            placeholder="name@site.com"
-          />
-          <FormErrorMessage>
-            {errors.email && errors.email.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={errors.pass && errors.pass.message}>
-          <FormLabel>Password</FormLabel>
-          <Input
-            aria-label="Password"
-            name="pass"
-            id="password"
-            type="password"
-            ref={register({
-              required: 'Please enter a password.'
-            })}
-          />
-          <FormErrorMessage>
-            {errors.pass && errors.pass.message}
-          </FormErrorMessage>
-        </FormControl>
         <Button
-          id="login"
-          type="submit"
+          onClick={() => auth.signinWithGitHub('/sites')}
           backgroundColor="gray.900"
           color="white"
-          isLoading={loading}
           fontWeight="medium"
-          mt={4}
+          leftIcon="github"
+          mt={2}
           h="50px"
-          fontSize="lg"
           _hover={{ bg: 'gray.700' }}
           _active={{
             bg: 'gray.800',
             transform: 'scale(0.95)'
           }}
         >
-          Login
+          Continue with GitHub
+        </Button>
+        <Button
+          onClick={() => auth.signinWithGoogle('/sites')}
+          backgroundColor="white"
+          color="gray.900"
+          variant="outline"
+          fontWeight="medium"
+          leftIcon="google"
+          mt={2}
+          h="50px"
+          _hover={{ bg: 'gray.100' }}
+          _active={{
+            bg: 'gray.100',
+            transform: 'scale(0.95)'
+          }}
+        >
+          Continue with Google
         </Button>
       </Stack>
     </Flex>
